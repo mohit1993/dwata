1d212352a4ea33899e8b0a773f7b08e5165958b9
Sumit Datta <sumitdatta@gmail.com>
2016-07-22 00:20:24 +0200

    Basic order by support

--- backend/handlers/read.py
+++ backend/handlers/read.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import sqlalchemy
 from sqlalchemy import create_engine, MetaData, select
 
 from common.config import extract_config
@@ -17,7 +18,23 @@ class ReadHandler(object):
             meta.reflect()
             if table in meta.tables:
                 tt = meta.tables[table]
-                exc = conn.execute(select([tt]))
+                sel = dict(
+                    columns=[tt]
+                )
+                if 'order_by' in request.params:
+                    order_by = request.params['order_by']
+                    order_by = [order_by, ] if type(order_by) is str else order_by
+                    valid_order_by = list()
+                    for x in order_by:
+                        ord_type = 'asc'
+                        if ':' in x:
+                            (x, ord_type) = x.split(':')[:2]
+                            ord_type = ord_type if ord_type in ('asc', 'desc') else 'asc'
+                        if x in tt.columns.keys():
+                            valid_order_by.append((x, ord_type))
+                    sel['order_by'] = (getattr(sqlalchemy, x[1])(getattr(tt.c, x[0])) for x in valid_order_by)
+
+                exc = conn.execute(select(**sel))
                 response.body = json.dumps(dict(
                         keys=exc.keys(),
                         results=exc.cursor.fetchall()
